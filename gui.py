46784ff7ef3c2e64c443f1a7361f4ca860a5115d
CCA2878 <1054331608@qq.com>
2022-03-04 23:44:47 +0800

    2022-3-4_23:44:36_AdjustGUI

--- gui.py
+++ gui.py
@@ -16,7 +16,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(427, 202)
+        MainWindow.resize(411, 191)
         # 禁止更改大小
         MainWindow.setFixedSize(MainWindow.width(), MainWindow.height())
 
@@ -24,27 +24,27 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(10, 20, 55, 16))
+        self.label.setGeometry(QRect(10, 10, 55, 16))
         self.snEdit = QLineEdit(self.centralwidget)
         self.snEdit.setObjectName(u"snEdit")
-        self.snEdit.setGeometry(QRect(40, 20, 381, 20))
+        self.snEdit.setGeometry(QRect(40, 10, 361, 20))
 
         self.pushButton = QPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(10, 50, 411, 24))
+        self.pushButton.setGeometry(QRect(10, 40, 391, 24))
 
         
 
         self.label_2 = QLabel(self.centralwidget)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(10, 80, 55, 16))
+        self.label_2.setGeometry(QRect(10, 70, 55, 16))
         self.pwEdit = QTextEdit(self.centralwidget)
         self.pwEdit.setObjectName(u"pwEdit")
-        self.pwEdit.setGeometry(QRect(10, 100, 411, 71))
+        self.pwEdit.setGeometry(QRect(10, 90, 391, 71))
         self.pwEdit.setReadOnly(True)
         self.label_3 = QLabel(self.centralwidget)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(10, 180, 411, 16))
+        self.label_3.setGeometry(QRect(10, 170, 411, 16))
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from gui import Ui_MainWindow
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 
+version = 'v1.00'
 
 def calc_Action(sn, obj):
     if sn != '':
@@ -14,6 +15,7 @@ if __name__ == '__main__':
     __qtObj = QMainWindow()
     MainGui = Ui_MainWindow()
     MainGui.setupUi(__qtObj)
+    __qtObj.setWindowTitle(__qtObj.windowTitle() +'_'+ version)
     __qtObj.setVisible(True)
     MainGui.pushButton.clicked.connect(lambda: calc_Action(MainGui.snEdit.text(), MainGui))
     sys.exit(__qtApp.exec())
